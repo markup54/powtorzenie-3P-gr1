4b45b8795279048d66754e673abd0ed74a90ccd3
markup54 <marzenakupka@tlen.pl>
2022-09-22 16:35:13 +0200

    liczby fibbonacciego

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -40,5 +41,33 @@ public class Main {
                 liczbyPierwsze.add(element);
         }
         System.out.println(liczbyPierwsze);
+
+        //przygotuj listę z 10 liczbami wylosowanym z zakresu od 30 do 60 bez powtorzen
+        HashSet<Integer> zbiorWylosowanych = new HashSet<>();
+        while(zbiorWylosowanych.size()<10){
+            zbiorWylosowanych.add((int)(Math.random()*30+31));
+        }
+        System.out.println(zbiorWylosowanych);
+        //tablica 10 liczb wypełniona pierwiastkami
+        double pierwiastki [] = new double[10];
+        for(int i=0 ; i<10;i++){
+            pierwiastki[i] = Math.pow(i,0.5);
+        }
+        System.out.println(pierwiastki);
+        for (double wartosc : pierwiastki){
+            System.out.print(wartosc+", ");
+        }
+
+        //liczby fibbonacciego
+        System.out.println("Liczby Fibbonacciego");
+        ArrayList<Integer> fibo = new ArrayList<>();
+        fibo.add(0);
+        fibo.add(1);
+        int indeks=2;
+        while (fibo.size()<40){
+            fibo.add (fibo.get(indeks-2)+fibo.get(indeks-1));
+            indeks++;
+        }
+        System.out.println(fibo);
     }
 }
